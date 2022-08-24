f2401eb1a0bfb290ef867b8a9200bdf3adc084b3
Praveen Salikanti <praveen_salikanti@epam.com>
2022-08-24 09:49:06 +0530

    initialupdate

---
+++ JenkinsDemo/JenkinsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
